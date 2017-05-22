d67da46d4bd5c1c92b8ab712445dae35d71e6d62
Tan Yu Xuan <yuxuan.tan.2014@sis.smu.edu.sg>
2017-05-22 14:12:37 +0800

    Completed Dashboard filter status

---
+++ LCWebApp/public_html/js/tableFilter.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    jobList = document.getElementById("jobList");
+    tr = jobList.getElementsByTagName('tr');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[4];
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
