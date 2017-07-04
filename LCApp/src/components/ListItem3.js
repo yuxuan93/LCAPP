b9de01faa4ffb8a288d263080a16d9d07823c0ed
Tan Yu Xuan <yuxuan.tan.2014@sis.smu.edu.sg>
2017-07-04 10:36:03 +0800

    [ ] FIX DASHBOARD AND HISTORY FILTER ALIGNMENT

---
+++ LCApp/src/components/ListItem3.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React, {Component} from 'react';
+import ReactNative from 'react-native';
+const styles = require('../styles/styles.js')
+const { View, TouchableHighlight, Text } = ReactNative;
+
+class ListItem extends Component {
+  render() {
+    return (
+      <TouchableHighlight onPress={this.props.onPress}>
+        <View style={styles.li}>
+          <Text style={styles.boldliText}>Job ID: {this.props.item.jobId}</Text>
+          <Text style={styles.liText}>Completed Date: {this.props.item.completeDate}</Text>
+          <Text style={styles.liText}>Address: {this.props.item.address}</Text>
+          <Text style={styles.liText}>Cust Name: {this.props.item.name}</Text>
+          
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+module.exports = ListItem;
